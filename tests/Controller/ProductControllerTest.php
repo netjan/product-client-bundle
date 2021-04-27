e395e4f1e4cf1b7c6d8505a802acbf501df0c2fa
Jan Czajkowski <janekcz69@gmail.com>
2021-04-27 10:53:31 +0200

    testing ProductController

--- tests/Controller/ProductControllerTest.php
+++ tests/Controller/ProductControllerTest.php
@@ -32,7 +32,7 @@ class ProductControllerTest extends WebTestCase
         $this->assertSame(404, $this->client->getResponse()->getStatusCode());
     }
 
-    public function testList()
+    public function testIndex()
     {
         $this->client->request('GET', '/client/product');
         $this->assertSame(301, $this->client->getResponse()->getStatusCode());
@@ -40,5 +40,116 @@ class ProductControllerTest extends WebTestCase
         $this->client->request('GET', '/client/product/');
         $this->assertResponseIsSuccessful();
         $this->assertSelectorTextContains('h2', 'Lita produktów');
+
+        $this->client->clickLink('Znajdują się na składzie');
+        $this->assertResponseIsSuccessful();
+
+        $this->client->request('GET', '/client/product/?stock=true');
+        $this->assertResponseIsSuccessful();
+        $this->assertSelectorTextContains('h2', 'Lita produktów');
+
+        $this->client->request('GET', '/client/product/?stock=false');
+        $this->assertResponseIsSuccessful();
+        $this->assertSelectorTextContains('h2', 'Lita produktów');
+    }
+
+    public function testNew()
+    {
+        $this->client->request('GET', '/client/product/new');
+        $this->assertResponseIsSuccessful();
+
+        $this->client->submitForm('Zapisz');
+        $this->assertResponseIsSuccessful();
+        $this->assertSelectorTextContains('li', 'This value should not be blank.');
+
+        $this->client->submitForm('Zapisz', [
+            'product[name]' => 'Fabien',
+            'product[amount]' => '',
+        ]);
+        $this->assertResponseIsSuccessful();
+        $this->assertSelectorTextContains('li', 'This value should not be blank.');
+
+        $this->client->submitForm('Zapisz', [
+            'product[name]' => '',
+            'product[amount]' => 6,
+        ]);
+        $this->assertResponseIsSuccessful();
+        $this->assertSelectorTextContains('li', 'This value should not be blank.');
+
+        $crawler = $this->client->submitForm('Zapisz', [
+            'product[name]' => 'Fabien',
+            'product[amount]' => 'asdd',
+        ]);
+        $this->assertResponseIsSuccessful();
+        $this->assertSelectorTextContains('li', 'This value is not valid.');
+
+        $this->client->submitForm('Zapisz', [
+            'product[name]' => 'Fabien',
+            'product[amount]' => -1,
+        ]);
+        $this->assertResponseIsSuccessful();
+        $this->assertSelectorTextContains('li', 'This value should be greater than or equal to 0.');
+
+        $this->client->submitForm('Zapisz', [
+            'product[name]' => 'Fabien',
+            'product[amount]' => 2,
+        ]);
+        $this->client->followRedirect();
+        $this->assertResponseIsSuccessful();
+    }
+
+    public function testShow()
+    {
+        $crawler = $this->client->request('GET', '/client/product/');
+        $this->assertResponseIsSuccessful();
+
+        $link = $crawler->selectLink('Podgląd')->link();
+        $this->client->click($link);
+        $this->assertResponseIsSuccessful();
+
+        $this->client->submitForm('Delete');
+        $this->client->followRedirect();
+        $this->assertResponseIsSuccessful();
+
+        $this->client->click($link);
+        $this->assertSame(404, $this->client->getResponse()->getStatusCode());
+    }
+
+    public function testEdit()
+    {
+        $crawler = $this->client->request('GET', '/client/product/');
+        $this->assertResponseIsSuccessful();
+
+        $link = $crawler->selectLink('Edycja')->link();
+        $crawler = $this->client->click($link);
+        $this->assertResponseIsSuccessful();
+
+        $form = $crawler->selectButton('Aktualizuj')->form();
+        $values = $form->getValues();
+
+        $form_data = [
+            'product[name]' => 'Fabien',
+            'product[amount]' => 100,
+        ];
+        $this->client->submit($form, $form_data);
+        $this->client->followRedirect();
+        $this->assertResponseIsSuccessful();
+
+        $crawler = $this->client->click($link);
+        $this->assertResponseIsSuccessful();
+        $newValues = $crawler->selectButton('Aktualizuj')->form()->getValues();
+
+        foreach ($form_data as $key => $value) {
+            $this->assertEquals($newValues[$key], $value);
+        }
+
+        $this->client->submit($form, [
+            'product[name]' => $values['product[name]'],
+            'product[amount]' => $values['product[amount]'],
+        ]);
+        $this->client->followRedirect();
+        $this->assertResponseIsSuccessful();
+
+
     }
 }
